842e3a9a1ad51336bb1dfbe46585ef9cee05d6a8
mollybair <mollybair@users.noreply.github.com>
2021-01-15 11:55:55 -0500

    Correlation tests

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # R-Code-Sample
+
+This code sample offers a simple analysis of the Work, Family, and Health Survey (WFHS). This survey 
+tests the effectiveness of an intervention that aims to reduce work-family conflict. While the WFHS Public Use Data is rather robust (though it does exclude measures of health outcomes), this analysis will focus on
+employee choice/agency and family-supportive supervisor behaviors. 
+
+The following questions guide this analysis:
+1. Is there a correlation between an employee's degree of choice over their work hours and 
+    the degree of family-supportive supervisor behaviors (FSSB) in their workplace?
+2. If such a correlation exists, how does the intervention affect it?
--- WFHS_Analysis.R
+++ WFHS_Analysis.R
@@ -15,6 +15,15 @@ names(tomo_df) <- tolower(names(tomo_df))
 names(leef_df) <- tolower(names(leef_df))
 
 tomo_df$condition <- ifelse(tomo_df$condition == 1, 1, 0)
+leef_df$condition <- ifelse(leef_df$condition ==1, 1, 0)
+
+### Recode Missing Values ###
+vars_numeric_missing <- c("wm_cwh1r", "wm_cwh2r", "wm_cwh3r", "wm_cwh4r", "wm_cwh5r", "wm_cwh6r",
+                          "wm_cwh7r", "wm_cwh8r", "wm_fssb1r", "wm_fssb3r", "wm_fssb4r", "wm_fssb5r")
+for (var in vars_numeric_missing) {
+  tomo_df[[var]] <- ifelse(tomo_df[[var]] < 1, NA, tomo_df[[var]])
+  leef_df[[var]] <- ifelse(leef_df[[var]] < 1, NA, leef_df[[var]])
+}
 
 ### Merge Data ###
 tomo_df$tomo <- 1
@@ -22,13 +31,34 @@ tomo_df$leef <- 0
 leef_df$leef <- 1
 leef_df$tomo <- 0
 wfhs_df <- rbind(tomo_df, leef_df)
-view(wfhs_df)
 
 ### Aggregate Grouped Survey Answers ###
-wfhs_df$employee_control <- wfhs_df$wm_cwh1r + wfhs_df$wm_cwh2r + wfhs_df$wm_cwh3r +
-  wfhs_df$wm_cwh4r + wfhs_df$wm_cwh5r + wfhs_df$wm_cwh6r + wfhs_df$wm_cwh7r + wfhs_df$wm_cwh8r
+wfhs_df$employee_control <- rowSums(subset(wfhs_df, select = wm_cwh1r:wm_cwh8r), na.rm = TRUE)
+  # higher value indicates employee has greater control over hours/schedule
+wfhs_df$fssb <- rowSums(subset(wfhs_df, select = wm_fssb1r:wm_fssb5r), na.rm = TRUE)
+  # higher value indicates greater family-supportive supervisor behavior (FSSB)
 
-wfhs_df$fssb <- wfhs_df$wm_fssb1r + wfhs_df$wm_fssb3r + wfhs_df$wm_fssb4r + wfhs_df$wm_fssb5r
+### Test Correlation Between Employee Choice and FSSB ###
+wfhs_pre_treat <- filter(wfhs_df, wave == 1)
+baseline_cor <- cor.test(wfhs_pre_treat$employee_control, wfhs_pre_treat$fssb, method = 'pearson')
+baseline_cor
+
+filter_cor_test <- function(df, new_df, val1, val2, cor_name) {
+  new_df <- filter(df, wave == val1, condition == val2)
+  cor_name <- cor.test(new_df$employee_control, new_df$fssb, method = 'pearson')
+  cor_name
+}
+
+filter_cor_test(wfhs_df, wfhs_treat_wave2, 2, 1, treat_wave2_cor) # treatment group, 6 month follow up
+filter_cor_test(wfhs_df, wfhs_treat_wave3, 3, 1, treat_wave3_cor) # treatment group, 12 month follow up
+filter_cor_test(wfhs_df, wfhs_treat_wave4, 4, 1, treat_wave4_cor) # treatment group, 18 month follow up
+
+filter_cor_test(wfhs_df, wfhs_control_wave2, 2, 0, control_wave2_cor) # control group, 6 month follow up
+filter_cor_test(wfhs_df, wfhs_control_wave3, 3, 0, control_wave3_cor) # control group, 12 month follow up
+filter_cor_test(wfhs_df, wfhs_control_wave4, 4, 0, control_wave4_cor) # control group, 18 month follow up
+
+
+
 
 
 
